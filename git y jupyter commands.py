84c8a38e2189965abff6531353f7ad0e66aac8dc
Mona Lisa <mario@Marios-MacBook-Air.local>
2020-07-27 19:07:56 -0500

    Hi

---
+++ git y jupyter commands.py
@@ -0,0 +1,9 @@
+git clone <sshurl>
+git status
+git add .
+git add <filename>
+git commit -m "message"
+git push origin <branch>
+
+jupyter notebook #para abrir jupyter
+conda install -c anaconda nb_conda_kernels #si no reconoce el PythonData
